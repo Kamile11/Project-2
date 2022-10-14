d61e9cfe25dac91326348cd5d7e8fdba13f736eb
Kamile11 <112688949+Kamile11@users.noreply.github.com>
2022-10-14 23:08:08 +0300

    Update header.h

--- header.h
+++ header.h
@@ -40,3 +40,17 @@ void rezultatai(data& a);
 //---
 
 const char CDfv[] = "kursiokai.txt";
+
+class Timer {
+private:
+	std::chrono::time_point<std::chrono::high_resolution_clock> start;
+public:
+	Timer() : start{ std::chrono::high_resolution_clock::now() } {}
+	void reset() {
+		start = std::chrono::high_resolution_clock::now();
+	}
+	double elapsed() const {
+		return std::chrono::duration<double>
+			(std::chrono::high_resolution_clock::now() - start).count();
+	}
+};
