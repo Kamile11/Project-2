418af2243de735cb81a801456586778ea759f050
Kamile11 <112688949+Kamile11@users.noreply.github.com>
2022-10-29 01:41:28 +0300

    Create mediana-list.cpp

---
+++ mediana-list.cpp
@@ -0,0 +1,20 @@
+#include "header.h"
+
+//void galutinismed(data& a, int& n)//su mediana
+//{
+//	double med;
+//	
+//	//a.sort();
+//
+//	if (n % 2 != 0)
+//	{
+//		med = (double)a.paz[n / 2];
+//	}
+//	else
+//	{
+//		med = (double)(a.paz[(n - 1) / 2] + a.paz[n / 2]) / 2;
+//	}
+//
+//	a.medrezult = 0.4 * med + 0.6 * a.egz;
+//
+//}
